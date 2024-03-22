ecaf2ba6e6906ab97330964b3590f9ca33142d26
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-03-22 00:55:06 +0000

    Ranks country origins of bands, ordered by the number of (non-unique) fans.

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,11 @@
 -- Imports and  ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) AS nb_fans
+-- Temporary table to store the aggregated fan counts per country
+CREATE TEMPORARY TABLE tmp_band_fans AS
+SELECT origin, COUNT(*) AS nb_fans
 FROM metal_bands
-GROUP BY origin
+GROUP BY origin;
+
+-- Rank the countries by the number of non-unique fans
+SELECT origin, nb_fans
+FROM tmp_band_fans
 ORDER BY nb_fans DESC;
