e9a1344b364aadcf5a1496da44357cff032a267f
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-05-07 11:43:45 +0000

    Create Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Cache class the uses redis"""
 
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -25,4 +25,42 @@ class Cache:
 
         # Store the data with the key
         self._redis.set(key, data)
+
         return key
+
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[int, str, bytes, float]:
+        """Convert data from redis to python equivalent
+        Args:
+            - key (str): The key to search for value in redis.
+            - fn (Callable=None): To covert data to python equivalent.
+        Return:
+            Data: Converted value from redis server.
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Converts values from redis to string.
+        Arg:
+            - key (str): The key to fetch data from redis.
+        Return:
+            Converted data"""
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """Convert Values from redis
+        Arg:
+            - key (str): The key to fetch data from redis.
+        Return:
+            Converted data.
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
